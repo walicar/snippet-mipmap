d57484f540d5c5fa5657f293a8e70ea7357fab70
Will Alicar <will@alicar.me>
2025-09-20 23:58:05 -0700

    feat: add makeMip

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,77 @@
-export const loadImage = async (url: string) => {
+export const mipCount = (width: number, height: number) => Math.log2(Math.max(width, height)) + 1;
+
+export interface Tex {
+    data: ImageDataArray,
+    width: number,
+    height: number,
+}
+
+type Color = Uint8ClampedArray<ArrayBuffer>;
+
+export const bilinearFilter = (tl: Color, tr: Color, bl: Color, br: Color, t1: number, t2: number): Color => {
+    const lerp = (a: number, b: number, t: number) => a + (b - a) * t;
+    const mix = (a: Color, b: Color, t: number) => a.map((v, i) => lerp(v, b[i], t));
+
+    const top = mix(tl, tr, t1);
+    const bot = mix(bl, br, t1);
+
+    return mix(top, bot, t2);
+}
+
+export const makeMip = (tex: Tex): Tex => {
+    const { data: src, width: srcWidth, height: srcHeight } = tex;
+    const dstWidth = Math.max(1, Math.floor(srcWidth / 2));
+    const dstHeight = Math.max(1, Math.floor(srcHeight / 2));
+    const length = dstWidth * dstHeight;
+    const dst: ImageDataArray = new Uint8ClampedArray(length * 4);
+
+    const getColor = (row: number, col: number) => {
+        const srcOffset = (row * srcWidth) * col * 4;
+        return src.subarray(srcOffset, srcOffset + 4);
+    }
+
+    // for each dstPixel, blend colors of its 4 surrounding pixels
+    for (let p = 0; p < length; p++) {
+        // get texel coord of dstPixel
+        const dstRow = p / length;
+        const dstCol = p % length;
+
+        // get center texcoord in dst
+        const dstU = (dstRow + 0.5) / dstWidth;
+        const dstV = (dstCol + 0.5) / dstHeight;
+
+        // get corresponding center texcoord in src
+        const srcU = dstU * srcWidth - 0.5;
+        const srcV = dstV * srcHeight - 0.5;
+
+        // get topleft texel coord 
+        const srcRow = Math.floor(srcU);
+        const srcCol = Math.floor(srcV);
+
+        // calculate how much to mix our colors
+        const t1 = srcU - srcRow;
+        const t2 = srcV - srcCol;
+
+        // get colors for each square
+        const tl = getColor(srcRow, srcCol);
+        const tr = getColor(srcRow + 1, srcCol);
+        const bl = getColor(srcRow, srcCol + 1);
+        const br = getColor(srcRow + 1, srcCol + 1);
+
+        // get filtered color
+        const dstColor = bilinearFilter(tl, tr, bl, br, t1, t2);
+        const dstOffset = (dstRow * dstWidth) * dstCol * 4;
+        dst.set(dstColor, dstOffset);
+    }
+
+    return { 
+        data: dst,
+        width: dstWidth,
+        height: dstHeight
+    }
+}
+
+export const loadImage = async (url: string): Promise<Tex> => {
     const res = await fetch(url);
     const blob = await res.blob();
     const bitmap = await createImageBitmap(blob, { colorSpaceConversion: "none" });
@@ -9,6 +82,6 @@ export const loadImage = async (url: string) => {
     if (!context) throw new Error("Unable to get 2d context");
     context.drawImage(bitmap, 0, 0);
 
-    const data = context.getImageData(0,0,width,height).data.buffer;
+    const data = context.getImageData(0,0,width,height).data;
     return { data, width, height };
 }
